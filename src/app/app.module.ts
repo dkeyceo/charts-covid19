b8e9e906e128a1e80eb1031accbea1da2b646eaa
dkeyceo <dkeyceo@gmail.com>
2023-09-05 21:49:20 +0300

    Described high-level design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { BottomComponent } from './bottom/bottom.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    PrincipalComponent,
+    BottomComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
